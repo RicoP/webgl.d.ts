50cf71acd2494a3759eec32531d19743c5cfe544
RicoP <mailtorico@gmail.com>
2012-10-08 20:30:29 +0200

    overload

--- README.md
+++ README.md
@@ -12,12 +12,14 @@ Example App
 		var canvas = <HTMLCanvasElement> document.createElement("canvas"); 
 		document.body.appendChild(canvas); 
 
-		var context : any = canvas.getContext("experimental-webgl"); 
-		var gl = <WebGLRenderingContext> context; 
+		var gl = canvas.getContext("experimental-webgl", {}); 
 
 		gl.clearColor(1,0,0,1);
 		gl.clear(gl.COLOR_BUFFER_BIT); 
 	}
+
+
+It's important to notice that the second parameter of getContext is mandatory so TypeScript can figure out that you want a WebGLContext and not a Canvas2DContext. 
 
 
 
--- webgl.d.ts
+++ webgl.d.ts
@@ -504,3 +504,15 @@ interface WebGLContextEvent extends Event {
 	initWebGLContextEvent(typeArg : string, canBubbleArg : bool, cancelableArg : bool, statusMessageArg : string) : void;
 }
 
+//Extend the window object with cross Browser callbacks so TS will not complain 
+interface Window {	
+    mozRequestAnimationFrame(callback: FrameRequestCallback): number;
+    oRequestAnimationFrame(callback: FrameRequestCallback): number;
+    webkitRequestAnimationFrame(callback: FrameRequestCallback): number;
+}
+
+//To make WebGL work 
+interface HTMLCanvasElement {
+    getContext(contextId: string, params : {}): WebGLRenderingContext;
+}
+
