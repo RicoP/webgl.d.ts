a688293113165cbf05a16c06299a41e41e4695d5
Rico P <MailToRico@googlemail.com>
2012-10-29 13:25:01 +0100

    added more parameter for requestAnimationFrame

--- webgl.d.ts
+++ webgl.d.ts
@@ -513,14 +513,22 @@ interface WebGLContextEvent extends Event {
 }
 
 //Extend the window object with cross Browser callbacks so TS will not complain 
-interface Window {	
-    mozRequestAnimationFrame(callback: FrameRequestCallback): number;
-    oRequestAnimationFrame(callback: FrameRequestCallback): number;
-    webkitRequestAnimationFrame(callback: FrameRequestCallback): number;
+//Also add the (non-standard) Canvas Element parameter for performance improvement 
+interface WindowAnimationTiming {
+	requestAnimationFrame(callback: FrameRequestCallback, canvas ?: HTMLCanvasElement): number;
+	msRequestAnimationFrame(callback: FrameRequestCallback, canvas ?: HTMLCanvasElement): number;
+	mozRequestAnimationFrame(callback: FrameRequestCallback, canvas ?: HTMLCanvasElement): number;
+	webkitRequestAnimationFrame(callback: FrameRequestCallback, canvas ?: HTMLCanvasElement): number;
+	oRequestAnimationFrame(callback: FrameRequestCallback, canvas ?: HTMLCanvasElement): number;	 
+
+	cancelRequestAnimationFrame(handle: number): void;
+	msCancelRequestAnimationFrame(handle: number): void;
+	mozCancelRequestAnimationFrame(handle: number): void;
+	webkitCancelRequestAnimationFrame(handle: number): void;
+	oCancelRequestAnimationFrame(handle: number): void;
 }
 
 //To make WebGL work 
 interface HTMLCanvasElement {
-    getContext(contextId: string, params : {}): WebGLRenderingContext;
+	getContext(contextId: string, params : {}): WebGLRenderingContext;
 }
-
